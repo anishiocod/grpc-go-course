a00d525575e9c4401fff4f8720ef5adeb4bd2b2c
anishiocod <anish@iocod.com>
2024-11-13 14:17:20 +0530

    basic client and server

---
+++ greet/client/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var addr string = "0.0.0.0:50051"
+
+func main() {
+	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect on:%v\n", err)
+
+	}
+	defer conn.Close()
+
+}
---
+++ greet/server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/anishiocod/grpc-go-course/greet/proto"
+	"google.golang.org/grpc"
+)
+
+var addr string = "0.0.0.0:50051"
+
+type Server struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("Failed to listen on:%v\n", err)
+
+	}
+	log.Printf("listening on %v", addr)
+
+	s := grpc.NewServer()
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve %v\n", err)
+	}
+
+}
